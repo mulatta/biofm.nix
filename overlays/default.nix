1c900df51d3b82b413e084f4a0cf842f3f7aadf6
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-05 12:55:54 +0900

    biotite: add to overlay with version alias

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,6 +2,8 @@ _final: prev: {
   pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
     (py-final: _py-prev: {
       ankh = py-final.callPackage ../packages/ankh/package.nix { };
+      biotite_0_39 = py-final.callPackage ../packages/biotite/package.nix { };
+      biotite = py-final.biotite_0_39;
       fair-esm = py-final.callPackage ../packages/fair-esm/package.nix { };
     })
   ];
